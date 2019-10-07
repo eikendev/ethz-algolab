e177dfe12b558c1af977d2d1f8aaeaf449264ee2
eikendev <raphael@eiken.dev>
2019-10-08 00:09:53 +0200

    Improve implementation for firsthit.
    This version is faster, but still not good enough to receive all points.

--- week3/firsthit/src/algorithm.cpp
+++ week3/firsthit/src/algorithm.cpp
@@ -1,5 +1,7 @@
+#include <algorithm>
 #include <cstdlib>
 #include <iostream>
+#include <vector>
 
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
 
@@ -23,14 +25,14 @@ double floor_to_double(const K::FT& x)
 void process_testcase(uint32_t n)
 {
 	R target;
+	vector<S> segments;
 
 	int64_t x1, y1, x2, y2;
 
 	cin >> x1 >> y1 >> x2 >> y2;
 	target = R(P(x1, y1), P(x2, y2));
 
-	P best_intersection;
-	K::FT best_distance;
+	S best_segment;
 	bool found = false;
 
 	for (uint32_t i = 0; i < n; i++) {
@@ -40,46 +42,42 @@ void process_testcase(uint32_t n)
 		P p2 = P(x2, y2);
 		S segment = S(p1, p2);
 
-		if (CGAL::do_intersect(target, segment)) {
-			auto o = CGAL::intersection(target, segment);
+		if (CGAL::do_intersect(target, segment))
+			segments.push_back(segment);
+	}
+
+	random_shuffle(segments.begin(), segments.end());
+
+	for (uint32_t i = 0; i < segments.size(); i++) {
+		S segment = segments[i];
+
+		if (found ? CGAL::do_intersect(best_segment, segment) : CGAL::do_intersect(target, segment)) {
+			auto o = found ? CGAL::intersection(best_segment, segment) : CGAL::intersection(target, segment);
 
 			if (const P *new_intersection = boost::get<P>(&*o)) {
-				K::FT new_distance;
-
-				new_distance = CGAL::squared_distance(*new_intersection, target.source());
-
-				if (!found || new_distance < best_distance) {
-					best_intersection = P(*new_intersection);
-					best_distance = new_distance;
-					found = true;
-				}
+				best_segment = S(target.source(), *new_intersection);
 			} else if (const S *new_intersection = boost::get<S>(&*o)) {
-				K::FT new_distance;
+				K::FT ds = CGAL::squared_distance(new_intersection->source(), target.source());
+				K::FT dt = CGAL::squared_distance(new_intersection->target(), target.source());
 
-				new_distance = CGAL::squared_distance(new_intersection->source(), target.source());
-
-				if (!found || new_distance < best_distance) {
-					best_intersection = P(new_intersection->source());
-					best_distance = new_distance;
-					found = true;
-				}
-
-				new_distance = CGAL::squared_distance(new_intersection->target(), target.source());
-
-				if (!found || new_distance < best_distance) {
-					best_intersection = P(new_intersection->target());
-					best_distance = new_distance;
-					found = true;
-				}
+				if (ds < dt)
+					best_segment = S(target.source(), new_intersection->source());
+				else
+					best_segment = S(target.source(), new_intersection->target());
 			} else {
 				throw std::runtime_error("Invalid segment intersection.");
 			}
+
+			found = true;
+
+			if (best_segment.target() == target.source())
+				break;
 		}
 	}
 
 	if (found) {
-		int64_t x = floor_to_double(best_intersection.x());
-		int64_t y = floor_to_double(best_intersection.y());
+		int64_t x = floor_to_double(best_segment.target().x());
+		int64_t y = floor_to_double(best_segment.target().y());
 		cout << x << " " << y << endl;
 	} else {
 		cout << "no" << endl;
