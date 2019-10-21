c2ce843c80733e9a3d4aa5695cf5ccd5d6c99532
eikendev <raphael@eiken.dev>
2019-10-21 15:42:48 +0200

    Improve implementation for light_at_the_museum.
    This patch sorts the switches before passing them to the evaluating
    function. Sorting is done by the number of lights a switch controls.
    
    Sorting allows the recursion to converge faster, thus leading to tighter
    bounds and a faster run-time.

--- week5/light_at_the_museum/src/algorithm.cpp
+++ week5/light_at_the_museum/src/algorithm.cpp
@@ -1,13 +1,22 @@
+#include <algorithm>
+#include <array>
 #include <cstdlib>
 #include <iostream>
+#include <numeric>
 #include <vector>
 
 using namespace std;
 
-typedef vector<vector<vector<uint16_t>>> Network;
+typedef array<uint16_t, 2> Room;
+typedef vector<Room> Switch;
+typedef vector<Switch> Network;
 
 static uint16_t operations;
 static bool success;
+
+static const auto SumSwitch = [](uint16_t acc, const Room& r) {
+	return acc + r[0] + r[1];
+};
 
 void min_operations(const vector<uint16_t>& target, const Network& switches, uint8_t flipped, uint8_t step, const vector<uint16_t>& minsums, const vector<uint16_t>& maxsums)
 {
@@ -15,11 +24,6 @@ void min_operations(const vector<uint16_t>& target, const Network& switches, uin
 	vector<uint16_t> minsums2(minsums);
 	vector<uint16_t> maxsums1(maxsums);
 	vector<uint16_t> maxsums2(maxsums);
-
-	bool minsums1_equal = true;
-	bool minsums2_equal = true;
-	bool maxsums1_equal = true;
-	bool maxsums2_equal = true;
 
 	bool minsums1_leq = true;
 	bool minsums2_leq = true;
@@ -35,11 +39,6 @@ void min_operations(const vector<uint16_t>& target, const Network& switches, uin
 		maxsums1[i] += switches[step][i][0] - maxsum;
 		maxsums2[i] += switches[step][i][1] - maxsum;
 
-		minsums1_equal = minsums1_equal and (minsums1[i] == target[i]);
-		minsums2_equal = minsums2_equal and (minsums2[i] == target[i]);
-		maxsums1_equal = maxsums1_equal and (maxsums1[i] == target[i]);
-		maxsums2_equal = maxsums2_equal and (maxsums2[i] == target[i]);
-
 		minsums1_leq = minsums1_leq and (minsums1[i] <= target[i]);
 		minsums2_leq = minsums2_leq and (minsums2[i] <= target[i]);
 		maxsums1_geq = maxsums1_geq and (maxsums1[i] >= target[i]);
@@ -50,10 +49,10 @@ void min_operations(const vector<uint16_t>& target, const Network& switches, uin
 	}
 
 	if (step == switches.size() - 1) {
-		if (minsums1_equal) {
+		if (minsums1_leq && maxsums1_geq) {
 			operations = flipped;
 			success = true;
-		} else if (minsums2_equal) {
+		} else if (minsums2_leq && maxsums2_geq) {
 			operations = flipped + 1;
 			success = true;
 		}
@@ -79,14 +78,28 @@ void process_test(void)
 	for (uint16_t i = 0; i < m; i++)
 		cin >> target[i];
 
-	Network switches(n, vector<vector<uint16_t>>(m, vector<uint16_t>(2)));
-	vector<uint16_t> minsums(target.size(), 0);
-	vector<uint16_t> maxsums(target.size(), 0);
+	Network switches(n, vector<Room>(m));
 
 	for (uint16_t i = 0; i < n; i++) {
 		for (uint16_t j = 0; j < m; j++) {
 			cin >> switches[i][j][0];
 			cin >> switches[i][j][1];
+		}
+	}
+
+	// Sort the entries by decreasing influence.
+	// Influence is the sum of all lights a switch controls.
+	sort(switches.begin(), switches.end(), [](const Switch& a, const Switch& b) {
+		uint16_t sum_a = accumulate(a.begin(), a.end(), (uint16_t) 0, SumSwitch);
+		uint16_t sum_b = accumulate(b.begin(), b.end(), (uint16_t) 0, SumSwitch);
+		return sum_a > sum_b;
+	});
+
+	vector<uint16_t> minsums(target.size(), 0);
+	vector<uint16_t> maxsums(target.size(), 0);
+
+	for (uint16_t i = 0; i < n; i++) {
+		for (uint16_t j = 0; j < m; j++) {
 			minsums[j] += min(switches[i][j][0], switches[i][j][1]);
 			maxsums[j] += max(switches[i][j][0], switches[i][j][1]);
 		}
