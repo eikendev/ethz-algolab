7b3e5bb72b52f581234e6ceb2207fbf34b55d6ef
eikendev <raphael@eiken.dev>
2019-12-11 20:05:41 +0100

    Adjust program template.

--- algolab.sh
+++ algolab.sh
@@ -46,11 +46,20 @@ CXX_TEMPLATE=$(cat <<'EOF'
 
 using namespace std;
 
-int main(int argc, char const *argv[])
+void process_test(void)
+{
+}
+
+int main(void)
 {
 	ios_base::sync_with_stdio(false);
 
-	cout << "Hello World!\n";
+	uint16_t t;
+
+	cin >> t;
+
+	for (uint16_t i = 0; i < t; i++)
+		process_test();
 
 	return EXIT_SUCCESS;
 }
