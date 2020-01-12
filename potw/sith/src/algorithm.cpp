769263921d3724f64aafd82d6014028702cd376b
eikendev <raphael@eiken.dev>
2020-01-12 01:41:09 +0100

    Add missing PotWs.

---
+++ potw/sith/src/algorithm.cpp
@@ -0,0 +1,22 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+void process_test(void)
+{
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+
+	uint16_t t;
+
+	cin >> t;
+
+	for (uint16_t i = 0; i < t; i++)
+		process_test();
+
+	return EXIT_SUCCESS;
+}
