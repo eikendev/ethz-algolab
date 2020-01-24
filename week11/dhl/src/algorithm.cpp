554a4cd7955b0048145242aec9bfecf7b312b06e
eikendev <raphael@eiken.dev>
2020-01-25 00:18:06 +0100

    Add dhl.

--- week11/dhl/src/algorithm.cpp
+++ week11/dhl/src/algorithm.cpp
@@ -1,10 +1,47 @@
 #include <cstdlib>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 void process_test(void)
 {
+	uint16_t n;
+	cin >> n;
+
+	vector<uint32_t> a(n);
+
+	for (uint16_t i = 0; i < n; i++) {
+		cin >> a[i];
+		a[i]--;
+	}
+
+	vector<uint32_t> b(n);
+
+	for (uint16_t i = 0; i < n; i++) {
+		cin >> b[i];
+		b[i]--;
+	}
+
+	vector<vector<uint32_t>> cost(n, vector<uint32_t>(n, 0));
+
+	cost[0][0] = a[0] * b[0];
+
+	for (uint16_t i = 1; i < n; i++) {
+		cost[i][0] = cost[i - 1][0] + a[i] * b[0];
+		cost[0][i] = cost[0][i - 1] + a[0] * b[i];
+	}
+
+	for (uint16_t i = 1; i < n; i++) {
+		for (uint16_t j = 1; j < n; j++) {
+			uint32_t a_extend = cost[i - 1][j];
+			uint32_t b_extend = cost[i][j - 1];
+			uint32_t no_extend = cost[i - 1][j - 1];
+			cost[i][j] = a[i] * b[j] + min(no_extend, min(a_extend, b_extend));
+		}
+	}
+
+	cout << cost[n - 1][n - 1] << endl;
 }
 
 int main(void)
